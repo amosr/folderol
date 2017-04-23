e9186746372d12760b5d761b1b21f81ac9891e76
Amos Robinson <amos.robinson@gmail.com>
2017-04-23 11:56:49 +1000

    Chunking channels

--- src/Folderol/Spawn.hs
+++ src/Folderol/Spawn.hs
@@ -5,6 +5,9 @@ module Folderol.Spawn where
 import qualified Folderol.Source as Source
 import qualified Folderol.Sink as Sink
 
+import qualified Data.Vector as Vector
+import qualified Data.Vector.Mutable as MVector
+
 import P
 
 import System.IO (IO)
@@ -16,31 +19,55 @@ class Monad m => Spawn m where
  channel :: m (Sink.Sink m a, Source.Source m a)
 
 instance Spawn IO where
+ {-# INLINE join2 #-}
  join2 a b = do
   ref <- newEmptyMVar
   _ <- forkFinally a (\_ -> putMVar ref ())
   b
   takeMVar ref
- {-# INLINE join2 #-}
 
+ {-# INLINE channel #-}
  channel = do
   chan <- newChan
-  let snk = Sink.Sink
-            { Sink.init = return chan
-            , Sink.push = \q v -> do
-                writeChan q (Just v)
-                return q
-            , Sink.done = \q -> do
-                writeChan q Nothing
-                return ()
-            }
-  let src = Source.Source
-            { Source.init = return chan
-            , Source.pull = \q -> do
-                v <- readChan q
-                return (v, q)
-            , Source.done = \_ -> return ()
-            }
+  return (sink chan, source chan)
+  where
+   {-# INLINE sink #-}
+   sink q = Sink.Sink
+    { Sink.init = (,) 0 <$> MVector.unsafeNew chunkSize
 
-  return (snk, src)
- {-# INLINE channel #-}
+    , Sink.push = \(ix,mv) x -> do
+       MVector.unsafeWrite mv ix x
+       let ix' = ix + 1
+       case ix' == chunkSize of
+        True -> do
+          v <- Vector.unsafeFreeze mv
+          writeChan q v
+          mv' <- MVector.unsafeNew chunkSize
+          return (0, mv')
+        False -> do
+          return (ix', mv)
+
+    , Sink.done = \(ix,mv) -> do
+       v <- Vector.unsafeFreeze $ MVector.unsafeSlice 0 ix mv
+       writeChan q v
+       writeChan q Vector.empty
+    }
+
+   source q = Source.Source
+    { Source.init = return (0, Vector.empty)
+
+    , Source.pull = \(ix,v) -> do
+       case ix < Vector.length v of
+        True -> return (Just $ Vector.unsafeIndex v ix, (ix + 1, v))
+        False -> do
+          v' <- readChan q
+          case Vector.null v' of
+           True  -> return (Nothing, (0,v'))
+           False -> return (Just $ Vector.unsafeIndex v' 0, (1, v'))
+
+    , Source.done = \_ -> return ()
+    }
+
+   -- TODO: this should be exposed / configurable
+   chunkSize = 2
+
