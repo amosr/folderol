0d7d731957682d75b91e14a492ceff06f103a960
Amos Robinson <amos.robinson@gmail.com>
2017-06-06 10:37:52 +1000

    stream representations: fix Pull

--- sketch/Pull.hs
+++ sketch/Pull.hs
@@ -1,58 +1,43 @@
-module Pull (xs) where
+module Pull  where
 
-newtype Stream a = Stream ( (a -> IO ()) -> IO () )
+import Data.IORef
 
-source :: [a] -> Stream a
-source xs = Stream $ \k ->
-  let go [] = return ()
-      go (x:xs) = do
-              k x
-              go xs
-      {-# INLINE go #-}
-  in go xs
+newtype Stream a = Stream ( IO (Maybe a) )
+
+source :: [a] -> IO (Stream a)
+source xs = do
+  ref <- newIORef xs
+  return $ Stream $ go ref
  where
+  go ref = do
+   ls <- readIORef ref
+   case ls of
+    [] -> return Nothing
+    (l:ls') -> do
+      writeIORef ref ls'
+      return (Just l)
 {-# INLINE source #-}
 
+
 zappend :: Stream a -> Stream a -> Stream a
-zappend (Stream k1) (Stream k2) = Stream $ \k -> do
-  k1 k
-  k2 k
+zappend (Stream s1) (Stream s2) = Stream go1
+ where
+  go1 = do
+   a <- s1
+   case a of
+    Nothing -> s2
+    Just _  -> return a
 
 zzip :: Stream a -> Stream b -> Stream (a,b)
-zzip (Stream k1) (Stream k2) = Stream $ \k -> go1 k
+zzip (Stream s1) (Stream s2) = Stream go
  where 
-  go1 k     = k1 (go2 k)
-  go2 k a   = k2 (go3 k a)
-  go3 k a b = do
-    k (a,b)
-    go1 k
-
-zzip2 :: Stream a -> Stream b -> Stream (a,b)
-zzip2 (Stream k1) (Stream k2) = Stream (\k ->
-  let go1     = k1 go2
-      go2 a   = k2 (go3 a)
-      go3 a b = do
-        k (a, b)
-        go1
-  in  go1)
+  go = do
+   a <- s1
+   b <- s2
+   case (a,b) of
+    (Just a', Just b') -> return (Just (a',b'))
+    (_      , _      ) -> return Nothing
 
 zmap :: (a -> b) -> Stream a -> Stream b
-zmap f (Stream s) = Stream $ \k -> s (k . f)
+zmap f (Stream s) = Stream ( fmap (fmap f) s)
 {-# INLINE zmap #-}
-
-zfilter :: (a -> Bool) -> Stream a -> Stream a
-zfilter f (Stream s) = Stream $ \k -> s (go k)
- where
-  go k a
-   | f a
-   = k a
-   | otherwise
-   = return ()
-{-# INLINE zfilter #-}
-
-zprint :: Show a => Stream a -> IO ()
-zprint (Stream s) = s print
-{-# INLINE zprint #-}
-
-xs = zprint $ zfilter (>2) $ zmap (+1) $ source [1 :: Int,2,3]
-
