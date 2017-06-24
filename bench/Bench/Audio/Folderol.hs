bd0d939d4aae85853f47adb506503328b55a2eb4
Amos Robinson <amos.robinson@gmail.com>
2017-06-24 12:53:04 +1000

    benchmark

--- bench/Bench/Audio/Folderol.hs
+++ bench/Bench/Audio/Folderol.hs
@@ -2,11 +2,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Bench.Audio.Folderol where
 import Bench.Audio.Audio
-import Bench.Plumbing.Folderol
+import qualified Bench.Plumbing.Folderol as Plumbing
 
-import Folderol
-import Folderol.Splice
-import Prelude hiding (filter, map, zipWith)
+import qualified Folderol.Typed   as Network
+import qualified Folderol.Process as Process
+import qualified Folderol.Splice  as Splice
 
 import qualified Folderol.Source as Source
 
@@ -15,32 +15,30 @@ import qualified Data.Vector.Unboxed as Unbox
 
 runCompressor :: Unbox.Vector Double -> IO (Unbox.Vector Double)
 runCompressor !xs = do
-  (ys,()) <- vectorAtMostIO (Unbox.length xs) $ \snkYs -> do
-    $$(fuse defaultFuseOptions $ do
-        x0      <- source [|| Source.sourceOfVector xs ||]
+  (ys,()) <- Plumbing.vectorAtMostIO (Unbox.length xs) $ \snkYs -> do
+    $$(Splice.fuse Splice.defaultFuseOptions $ do
+        x0      <- Network.source    [|| Source.sourceOfVector xs ||]
 
-        squares <- map [|| \x -> x * x ||] x0
-        avg     <- postscanl [|| expAvg ||] [||0||] squares
-        root    <- map [|| clipRoot ||] avg
+        squares <- Process.map       [|| \x -> x * x ||]         x0
+        avg     <- Process.postscanl [|| expAvg      ||] [||0||] squares
+        root    <- Process.map       [|| clipRoot    ||]         avg
+        out     <- Process.zipWith   [|| (*)         ||] root    x0
 
-        out <- zipWith [||(*)||] root x0
-
-        sink out [|| snkYs ||])
+        Network.sink out [|| snkYs ||])
   return ys
 
 
 runCompressorLop :: Unbox.Vector Double -> IO (Unbox.Vector Double)
 runCompressorLop !xs = do
-  (ys,()) <- vectorAtMostIO (Unbox.length xs) $ \snkYs -> do
-    $$(fuse defaultFuseOptions $ do
-        x0      <- source [|| Source.sourceOfVector xs ||]
-        x'      <- postscanl [|| lopass ||] [||0||] x0
+  (ys,()) <- Plumbing.vectorAtMostIO (Unbox.length xs) $ \snkYs -> do
+    $$(Splice.fuse Splice.defaultFuseOptions $ do
+        x0      <- Network.source    [|| Source.sourceOfVector xs ||]
 
-        squares <- map [|| \x -> x * x ||] x'
-        avg     <- postscanl [|| expAvg ||] [||0||] squares
-        root    <- map [|| clipRoot ||] avg
+        x'      <- Process.postscanl [|| lopass      ||] [||0||] x0
+        squares <- Process.map       [|| \x -> x * x ||]         x'
+        avg     <- Process.postscanl [|| expAvg      ||] [||0||] squares
+        root    <- Process.map       [|| clipRoot    ||]         avg
+        out     <- Process.zipWith   [|| (*)         ||] root    x'
 
-        out <- zipWith [||(*)||] root x'
-
-        sink out [|| snkYs ||])
+        Network.sink out [|| snkYs ||])
   return ys
