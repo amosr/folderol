40041f9a6e4b51c79d51c9eef94e49ebb9dd27bf
Amos Robinson <amos.robinson@gmail.com>
2018-02-04 13:27:56 +1100

    implement chunking for reading file

--- bench/Bench/Correlation/Queries.hs
+++ bench/Bench/Correlation/Queries.hs
@@ -27,28 +27,29 @@ count :: Channel a -> Network IO (Channel Int)
 count stock = do
   F.fold [||\(!a) _ -> a + 1||] [||0 :: Int||] stock
 
+sourceRecords :: Haskell.TExpQ FilePath -> Network IO (Channel Record)
+sourceRecords fp = do
+  lined <- F.source [||sourceLinesOfFileChunked $$fp||]
+  records <- F.map    [||readRecordUnsafe||] lined
+
+  return records
 
 q'count :: Haskell.TExpQ FilePath -> Network IO ()
 q'count fpStock = do
-  stock0 <- F.source [|| sourceLinesOfFile $$fpStock ||]
-  stock  <- F.map    [||readRecordUnsafe||] stock0
+  stock  <- sourceRecords fpStock
   pot    <- count stock
   F.sink pot [||perform print||]
 
 q1 :: Haskell.TExpQ FilePath -> Haskell.TExpQ (Sink IO Double) -> Network IO ()
 q1 fpStock snkC1 = do
-  stock0 <- F.source [|| sourceLinesOfFile $$fpStock ||]
-  stock  <- F.map    [||readRecordUnsafe||] stock0
+  stock  <- sourceRecords fpStock
   pot    <- priceOverTime stock
   F.sink pot snkC1
 
 q2 :: Haskell.TExpQ FilePath -> Haskell.TExpQ FilePath -> Haskell.TExpQ (Sink IO Double) -> Haskell.TExpQ (Sink IO Double) -> Network IO ()
 q2 fpStock fpMarket snkC1 snkC2 = do
-  stock0 <- F.source [|| sourceLinesOfFile $$fpStock ||]
-  stock  <- F.map    [||readRecordUnsafe||] stock0
-
-  market0<- F.source [|| sourceLinesOfFile $$fpMarket ||]
-  market <- F.map    [||readRecordUnsafe||] market0
+  stock  <- sourceRecords fpStock
+  market <- sourceRecords fpMarket
 
   pot    <- priceOverTime stock
   pom    <- priceOverMarket stock market
@@ -59,11 +60,8 @@ q2 fpStock fpMarket snkC1 snkC2 = do
 
 q3 :: Haskell.TExpQ FilePath -> Haskell.TExpQ FilePath -> Haskell.TExpQ (Sink IO Double) -> Haskell.TExpQ (Sink IO Double) -> Haskell.TExpQ (Sink IO Double) -> Network IO ()
 q3 fpStock fpMarket snkC1 snkC2 snkC3 = do
-  stock0 <- F.source [|| sourceLinesOfFile $$fpStock ||]
-  stock  <- F.map    [||readRecordUnsafe||] stock0
-
-  market0<- F.source [|| sourceLinesOfFile $$fpMarket ||]
-  market <- F.map    [||readRecordUnsafe||] market0
+  stock  <- sourceRecords fpStock
+  market <- sourceRecords fpMarket
 
   pot    <- priceOverTime stock
   pom    <- priceOverMarket stock market
--- bench/Bench/Plumbing/Folderol.hs
+++ bench/Bench/Plumbing/Folderol.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
 module Bench.Plumbing.Folderol where
 
 import qualified Folderol.Source as Source
@@ -33,6 +34,62 @@ sourceLinesOfFile f = Source.Source
  , Source.done = \h -> do
     IO.hClose h
  }
+
+{-
+sourceChunksOfFile :: FilePath -> Source.Source IO ByteString.ByteString
+sourceChunksOfFile f = Source.Source
+ { Source.init = IO.openFile f IO.ReadMode
+ , Source.pull = \h -> do
+    e <- IO.hIsEOF h
+    case e of
+     False -> do
+      l <- Char8.hGetSome h 4096
+      return (Just l, h)
+     True -> do
+      return (Nothing, h)
+ , Source.done = \h -> do
+    IO.hClose h
+ }
+-}
+
+-- Not the default, because I can't be bothered implementing this for all the different libraries (Conduit, Streaming etc).
+-- But maybe I should implement it.
+sourceLinesOfFileChunked :: FilePath -> Source.Source IO ByteString.ByteString
+sourceLinesOfFileChunked f = Source.Source
+ { Source.init = do
+    h <- IO.openFile f IO.ReadMode
+    return (Char8.empty, h)
+ , Source.pull = \(buf,h) -> case takeLine buf of
+     Just (line,rest) ->
+      return (Just line, (rest,h))
+     Nothing ->
+      puller buf h
+ , Source.done = \(_,h) -> do
+    IO.hClose h
+ }
+ where
+  puller leftover h = do
+    e <- IO.hIsEOF h
+    case e of
+     True
+      | Char8.null leftover ->
+        return (Nothing, (leftover,h))
+      | otherwise ->
+        return (Just leftover, (Char8.empty,h))
+     False -> do
+      buf' <- Char8.hGetSome h 4096
+      case takeLine buf' of
+        Just (line,rest) -> return (Just (leftover `mappend` line), (rest, h))
+        Nothing -> puller (leftover `mappend` buf') h
+
+{-# INLINE takeLine #-}
+takeLine :: ByteString.ByteString -> Maybe (ByteString.ByteString, ByteString.ByteString)
+takeLine b =
+ case Char8.elemIndex '\n' b of
+  Nothing -> Nothing
+  Just ix -> Just $ Char8.splitAt (ix + 1) b
+
+
 
 sinkFileOfLines :: FilePath -> Sink.Sink IO ByteString.ByteString
 sinkFileOfLines f = Sink.Sink
